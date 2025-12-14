3689179789c65ed602756c72e47673d1f8f8f340
Chioma <okunborabigail@gmail.com>
2025-12-14 19:28:45 +0000

    Add random quote display functionality

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,1 +1,33 @@
+const quotes = [
+  {
+    text:"The best way to predict the future is to create it.",
+    category: "Motivation"
+  },
+  {
+    text:"Talk is cheap. Show me the code.",
+    category: "Programming"
+  },
+  {
+  text: "Healing is not linear.",
+    category: "Life"
+  }
+  ];
+
+const quoteDisplay = document.getElementById("quoteDisplay");
+const newQuoteBtn = document.getElementById("newQuote");
+const formContainer = document.getElementById("formContainer");
+  
+
+function sh quotes.showRandomQuote() {
+  quoteDisplay.innerHTML = "";
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  const quoteText = document.createElement("p)
+                                           quoteText.textContent = '"${randomQuote.text}"';
+  const quoteCategory = document.createElement("small");
+  quoteCategory.textContent = 'Category: ${randomQuote.category}';
+  quoteDisplay.appendChild(quoteText);
+  quoteDisplay.appendChild{quoteCategory};
+}
+newQuoteBtn.addEventListener("click", showRandomQuote);
 
